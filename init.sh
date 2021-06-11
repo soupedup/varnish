41d135ff15f53aff815429bca44b9f0fff9bf811
Joshua Sierles <joshua@chatterbug.com>
2021-06-11 20:31:14 +0200

    require setting BACKEND_PORT and BACKEND_HOST

--- init.sh
+++ init.sh
@@ -9,6 +9,16 @@ else
   VARNISH_SECRET_OPTION="-S /etc/varnish/secret"
 fi
 
+if [ -z "${BACKEND_PORT} ]; then
+  echo "Error: BACKEND_PORT must be set to the internal port of your backend application"
+  exit 1
+fi
+
+if [ -z "${BACKEND_HOST} ]; then
+  echo "Error: BACKEND_HOST must be set to the hostname resolving to your backend application instance IPs"
+  exit 1
+fi
+
 gomplate -f /etc/varnish/config.vcl.tpl > /etc/varnish/default.vcl
 
 exec varnishd \
